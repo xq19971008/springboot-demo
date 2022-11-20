2483d727a14841023f14c41c3a605069b83ae0fd
xiaoqiang <976677278@qq.com>
2022-11-20 23:13:28 +0800

    springboot-demo项目 v2

---
+++ src/main/java/com/kuang/config/MyMvcConfig.java
@@ -0,0 +1,17 @@
+package com.kuang.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+// 扩展使用SpringMVC
+@Configuration
+public class MyMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/index.html").setViewName("index");
+        registry.addViewController("/main.html").setViewName("dashboard");
+    }
+}
