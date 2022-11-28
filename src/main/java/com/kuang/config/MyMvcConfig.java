b6322a4f9d03fff519b0b37196b5dcd02ba67132
xiaoqiang <976677278@qq.com>
2022-11-28 22:21:13 +0800

    springboot-demo项目  多语言学习

--- src/main/java/com/kuang/config/MyMvcConfig.java
+++ src/main/java/com/kuang/config/MyMvcConfig.java
@@ -1,6 +1,8 @@
 package com.kuang.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -13,5 +15,10 @@ public class MyMvcConfig implements WebMvcConfigurer {
         registry.addViewController("/").setViewName("index");
         registry.addViewController("/index.html").setViewName("index");
         registry.addViewController("/main.html").setViewName("dashboard");
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        return new MyLocaleResolver();
     }
 }
