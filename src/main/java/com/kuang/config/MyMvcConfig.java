e56d6d2bd409dbde586f3bd574fff0b995012975
xiaoqiang <976677278@qq.com>
2022-12-05 22:18:24 +0800

    员工管理系统列表

--- src/main/java/com/kuang/config/MyMvcConfig.java
+++ src/main/java/com/kuang/config/MyMvcConfig.java
@@ -3,6 +3,7 @@ package com.kuang.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -20,5 +21,14 @@ public class MyMvcConfig implements WebMvcConfigurer {
     @Bean
     public LocaleResolver localeResolver() {
         return new MyLocaleResolver();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        // 注册拦截器，及拦截请求和要剔除哪些请求!
+        // 我们还需要过滤静态资源文件，否则样式显示不出来
+        registry.addInterceptor(new LoginHandlerInterceptor())
+                .addPathPatterns("/**")
+                .excludePathPatterns("/index.html","/user/login","/","/css/*","/img/**","/js/**");
     }
 }
